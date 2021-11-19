c0011b2dfa59b05738473ce7de467129b4bd9de2
Paulo Hilton <pahbaracho@gmail.com>
2021-11-19 12:21:05 -0300

    Cria os Loops e Displays Necessários

--- sketch.js
+++ sketch.js
@@ -15,33 +15,37 @@ function setup() {
   ground = new Ground(width/2,height,width,20);
 
   //criar objetos de divisão
-  for (var k = 0; k <=80; k = k + 80) {
+  for (var k = 10; k <=1000; k = k + 65) {
     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
   }
 
   //crie a 1ª linha de objetos plinko
-  for (var j = 75; j <=width; j=j+50) { 
+  for (var j = 55; j <=width; j=j+50) { 
     plinkos.push(new Plinko(j,75));
   }
 
   //crie a 2ª linha de objetos plinko
-  for (var j = 50; j <=width-10; j=j+50) 
+  for (var j = 30; j <=width-10; j=j+50) 
   {
     plinkos.push(new Plinko(j,175));
   }
 
   //crie a 3ª linha de objetos plinko
-
+  for (var j = 50; j <= width-20; j=j+50) {
+    plinkos.push(new Plinko(j,275))
+  }
   
   //crie a 4ª linha de objetos plinko
-
+  for (var j = 30; j <= width-30; j=j+50) {
+    plinkos.push(new Plinko(j,375))
+  }
 
   //criar objetos de partículas
   
-    
+  if(frameCount % 60 === 0) {
+    particles.push(new Particle(random(width/2-10, width/2+10),10,10))
+  }
 }
- 
-
 
 function draw() {
   background("black");
@@ -62,4 +66,7 @@ function draw() {
 
   //exibir as partículas
 
+  for (var p = 0; p < particles.length; k++) {
+    particles[p].display();
+  }
 }
