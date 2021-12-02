47859039ee53b6ea7e1ca3102b7f3f7a355f0d31
Paulo Hilton <pahbaracho@gmail.com>
2021-12-02 19:50:42 -0300

    Adicionei as Especificações Para que as Partículas Gerarem Randomicamente

--- sketch.js
+++ sketch.js
@@ -14,59 +14,46 @@ function setup() {
   world = engine.world;
   ground = new Ground(width/2,height,width,20);
 
-  //criar objetos de divisão
-  for (var k = 10; k <=1000; k = k + 65) {
-    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
-  }
-
-  //crie a 1ª linha de objetos plinko
-  for (var j = 55; j <=width; j=j+50) { 
-    plinkos.push(new Plinko(j,75));
-  }
-
-  //crie a 2ª linha de objetos plinko
-  for (var j = 30; j <=width-10; j=j+50) 
-  {
-    plinkos.push(new Plinko(j,175));
-  }
-
-  //crie a 3ª linha de objetos plinko
-  for (var j = 50; j <= width-20; j=j+50) {
-    plinkos.push(new Plinko(j,275))
-  }
-  
-  //crie a 4ª linha de objetos plinko
-  for (var j = 30; j <= width-30; j=j+50) {
-    plinkos.push(new Plinko(j,375))
-  }
-
-  //criar objetos de partículas
-  
-  if(frameCount % 60 === 0) {
-    particles.push(new Particle(random(width/2-10, width/2+10),10,10))
-  }
+    for (var k = 0; k <=width; k = k + 80) {
+      divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+    }
+    for (var j = 75; j <=width; j=j+50) 
+    {
+       plinkos.push(new Plinko(j,75));
+    }
+    for (var j = 50; j <=width-10; j=j+50) 
+    {
+       plinkos.push(new Plinko(j,175));
+    }
+     for (var j = 75; j <=width; j=j+50) 
+    {
+       plinkos.push(new Plinko(j,275));
+    }
+     for (var j = 50; j <=width-10; j=j+50) 
+    {
+       plinkos.push(new Plinko(j,375));
+    } 
 }
 
 function draw() {
   background("black");
   textSize(20)
- 
+ //text("Score : "+score,20,30);
   Engine.update(engine);
   ground.display();
   
-  //exibir os plinkos
-  for (var i = 0; i < plinkos.length; i++) {
-    plinkos[i].display();   
-  }
-   
-  //exibir os divisões
-  for (var k = 0; k < divisions.length; k++) {
-    divisions[k].display();
-  }
-
-  //exibir as partículas
-
-  for (var p = 0; p < particles.length; k++) {
-    particles[p].display();
-  }
+   for (var i = 0; i < plinkos.length; i++) { 
+     plinkos[i].display();
+   }
+   if(frameCount%60===0){
+     particles.push(new Particle(Math.round(random(width/2-100, width/2+100)), 10,10));
+     score++;
+   }
+ 
+  for (var j = 0; j < particles.length; j++) {
+     particles[j].display();
+   }
+   for (var k = 0; k < divisions.length; k++) {
+     divisions[k].display();
+   }
 }
